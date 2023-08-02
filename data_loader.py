97fe5a5e394f3cab22a86cacae734b57f84e0ae1
N <thorbencomes@protonmail.com>
2023-08-02 18:05:53 +0200

    adapted data loader to work on cluster

--- data_loader.py
+++ data_loader.py
@@ -1,13 +1,13 @@
 import pickle
-from unipath import Path
+# from unipath import Path
 
 
-path = Path('E:\\uni kram\\BA_NEW\\DATA')
+# path = Path('E:\\uni kram\\BA_NEW\\DATA')
 
 
 def load_pickled(name):
     # load_path = path + "\\" + name
-    load_path = "DATA\\" + name
+    load_path = "DATA/" + name
     with open(load_path, 'rb') as f:
         data = pickle.load(f)
         return data
