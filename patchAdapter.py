be3f142a9317a03653084fdfe6a1a803ff0e531d
N <thorbencomes@protonmail.com>
2023-08-03 17:16:25 +0200

    finished dictionary translation

--- patchAdapter.py
+++ patchAdapter.py
@@ -1,52 +1,58 @@
 from argparse import Namespace
 import torch.cuda
+import PatchTST.PatchTST_supervised.models.PatchTST
 
-def translate_dict_actions(config: dict):
+
+def translate_dict_actions(config: dict, data_dim):
     args = Namespace(
 
         # Forecasting task
-        seq_len = config["lookback_window"] + config["prediction_length"], # input sequence length
-        label_len= config["overlap"] + config["prediction_length"], # start token length
-        pred_len = config["prediction_length"],
+        seq_len=config["lookback_window"] + config["prediction_length"],  # input sequence length
+        label_len=config["overlap"] + config["prediction_length"],  # start token length
+        pred_len=config["prediction_length"],
 
-        # Linear
-        individual= config["individual"],
-
-        #PatchTST
-        fc_dropout=, # fully connected dropout
-        head_dropout = , # head dropout
-        patch_len = , # patch length
-        stride = , # stride
-        padding_patch = , # None: None; end: padding on the end
-        revin = , # RevIN; True 1 False 0
-        affine = , # RevIN-affine; True 1 False 0
-        subtract_last = , # 0: subtract mean; 1: subtract last
-        decomposition = , # decomposition; True 1 False 0
-        kernel_size = , # decomposition-kernel
-        e_layers = , # individual head; True 1 False 0
+        # PatchTST
+        fc_dropout=config["fc_dropout"],  # fully connected dropout
+        head_dropout=config["head_dropout"],  # head dropout
+        patch_len=config["patch_length"],  # patch length
+        stride=config["stride"],  # stride
+        padding_patch=config["padding_patch"],  # None: None; end: padding on the end
+        revin=config["revin"],  # RevIN; True 1 False 0
+        affine=config["affine"],  # RevIN-affine; True 1 False 0
+        subtract_last=config["subtract_last"],  # 0: subtract mean; 1: subtract last
+        decomposition=config["decomposition"],  # decomposition; True 1 False 0
+        kernel_size=config["kernel_size"],  # decomposition-kernel
+        individual=config["individual"],  # individual head; True 1 False 0
 
         # Formers
-        embed_type = , # 0: default 1: value embedding + temporal embedding + positional embedding 2: value embedding + temporal embedding 3: value embedding + positional embedding 4: value embedding
-        enc_in = config["lookback_window"],  # encoder input size
-        dec_in = config["overlap"] + config["prediction_length"],
-        c_out = , # output size
-        d_model = config["d_model"], # model dimension
-        n_heads = config["n_heads"], # number of heads
-        e_layers = config["encoder_layers"], # num of encoder layers
-        d_layers = config["decoder_layers"], # number of decoder layers
-        d_ff = , # dimension of fcn
-        moving_avg = 25, # window size moving average
-        factor = , # attention factor
-        distil = , # whether to use distilling in encoder, using this argument means not using distilling
+        embed_type=0,
+        # 0: default 1: value embedding + temporal embedding + positional embedding 2: value embedding + temporal
+        # embedding 3: value embedding + positional embedding 4: value embedding
+        enc_in=data_dim,  # enc input data dimension (for embedding)
+        dec_in=data_dim,
+        c_out=config["output_size"],  # output size
+        d_model=config["d_model"],  # model dimension
+        n_heads=config["n_heads"],  # number of heads
+        e_layers=config["encoder_layers"],  # num of encoder layers
+        d_layers=config["decoder_layers"],  # number of decoder layers
+        d_ff=config["d_fcn"],  # dimension of fcn
+        moving_avg=config["moving_average"],  # window size moving average
+        factor=1,  # attention factor
+        distil=config["distilling"],
+        # whether to use distilling in encoder, using this argument means not using distilling
         dropout=config["dropout"],
-        embed = , # time features encoding, options:[timeF, fixed, learned]
-        activation = , # activation function
-        output_attention = , # wether to output attention in ecoder
-        do_predict = , # whether to predict unseen future data
+        embed=config["embed"],  # time features encoding, options:[timeF, fixed, learned]
+        activation=config["activation"],  # activation function
+        output_attention=config["output_attention"],  # whether to output attention in encoder
+        # do_predict = , # whether to predict unseen future data
 
         # misc
-        use_gpu = True if torch.cuda.is_available() else False,
+        use_gpu=True if torch.cuda.is_available() else False,
     )
+
+    return args
 
 
 def patchtst(config: dict):
+    model = PatchTST.PatchTST_supervised.models.PatchTST.Model(config)
+    return model
