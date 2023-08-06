db265412549bc253c2f636533445523ea145948c
N <thorbencomes@protonmail.com>
2023-08-07 00:02:33 +0200

    added optional gpu mem monitoring

--- main_cluster.py
+++ main_cluster.py
@@ -40,19 +40,20 @@ class HyperparamOpt(experiment.AbstractIterativeExperiment):
         print("DEBUG: Experiment initialization completed")
 
     def finalize(self, surrender: ExperimentSurrender = None, crash: bool = False):
+        # self.iterator.finalize()
         # TODO: log prediction for visualization
         pass
 
     def iterate(self, cw_config: dict, rep: int, n: int) -> dict:
+        if False:
+            self.h3 %= 300
 
-        self.h3 %= 300
+            if self.h3 == 0:
+                print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024))
+                print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024))
+                print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024))
 
-        if self.h3 == 0:
-            print("torch.cuda.memory_allocated: %fGB" % (torch.cuda.memory_allocated(0) / 1024 / 1024 / 1024))
-            print("torch.cuda.memory_reserved: %fGB" % (torch.cuda.memory_reserved(0) / 1024 / 1024 / 1024))
-            print("torch.cuda.max_memory_reserved: %fGB" % (torch.cuda.max_memory_reserved(0) / 1024 / 1024 / 1024))
-
-        self.h3 += 1
+            self.h3 += 1
 
         with torch.device("cuda:0"):
             return self.iterator.iterate()
