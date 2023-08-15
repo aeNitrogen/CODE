d4a1c4bdd03eede46dfc00510c152032909c4edc
N <thorbencomes@protonmail.com>
2023-08-15 18:31:04 +0200

    moved prediction of patchtst to its iterator

--- iterators/PatchTST_Iterator.py
+++ iterators/PatchTST_Iterator.py
@@ -1,5 +1,6 @@
 import utils_ba
-
+import torch
+import data_plotter
 
 def optimize(model, optimizer, data, batch_size, data_dim, out_dim, criterion, pred_len, seq_len):
 
@@ -21,3 +22,45 @@ def optimize(model, optimizer, data, batch_size, data_dim, out_dim, criterion, p
             optimizer.step(closure)
 
     split_prediction_single()
+
+
+def predict(model, data, batch_size, data_dim, out_dim, criterion, mae_crit, pred_len, seq_len, final=False):
+    x, y = utils_ba.get_single(data, pred_len, seq_len, data_dim, out_dim)
+
+    # out = self.model.forward(x)
+    out = utils_ba.split_prediction_single(model, x, batch_size)
+
+    out = out[:, :, data_dim - out_dim:]
+    loss = criterion(out, y)
+    mae = mae_crit(out, y)
+
+    rmse = torch.sqrt(loss)
+    rmae = torch.sqrt(mae)
+
+    ret_dict = {
+        'validation_loss': loss,
+        'rmse': rmse,
+        'mae': mae,
+        'rmae': rmae
+    }
+
+    if final:
+
+        truth = torch.squeeze(data[0, :, :])
+        x, _ = utils_ba.get_single_pred(data, pred_len, seq_len, data_dim, out_dim)
+        prediction = utils_ba.split_prediction_single(model, x, batch_size)
+
+        zeros = torch.zeros_like(data[0, :, :])
+        prediction = prediction[0, :, :]
+        zeros[seq_len: pred_len + seq_len, :] = prediction
+        prediction = torch.squeeze(zeros)
+
+        update_dict = data_plotter.log_pred_plots(prediction.cpu(), truth.cpu())
+
+        ret_dict.update(update_dict)
+
+        update_dict = data_plotter.get_error_over_time(out, y, out_dim)
+
+        ret_dict.update(update_dict)
+
+    return ret_dict
--- training_iterator.py
+++ training_iterator.py
@@ -1,12 +1,15 @@
 import torch
+
+import models.PatchTST
 import wandb
 
-import transformer
 import data_loader
 import patchAdapter
 import numpy as np
 import utils_ba
 import torch.optim
+import iterators.PatchTST_Iterator
+import iterators.Autoformer_Iterator
 
 import data_plotter
 
@@ -15,6 +18,8 @@ class training_iterator:
     def __init__(self, config: dict):
         print("DEBUG: iterator initialization")
         self.architecture = config["architecture"]
+
+        self.batch_size = config["gpu_batch_size"]
 
         seed = config["seed"]
         torch.manual_seed(seed)
@@ -27,25 +32,32 @@ class training_iterator:
         config_translated = patchAdapter.translate_dict_actions(config, self.data_dim)
 
         print("DEBUG: config translated")
-
+        assert config["kernel_size"] % 2 == 1, "please choose odd kernel_size"
+        assert config["embed_type"] == 3 or config["embed_type"] == 4, "please choose another embed_type"
         self.overlap = config["overlap"]
         self.pred_len = config["prediction_length"]
         self.seq_len = config["lookback_window"]
-        self.out_dim = config["output_size"]
+        self.out_dim = config["real_output_size"]
         self.iterations = config["iterations"]
         self.iter = 0
         self.model = None
 
         if self.architecture == "transformer":
             print("TODO")
-            self.model = transformer.Transformer
+            # self.model = transformer.Transformer
 
         elif self.architecture == "PatchTST":
             self.model = patchAdapter.patchtst(config_translated)
+
+        elif self.architecture == "Autoformer":
+            self.model = patchAdapter.autoformer(config_translated)
+
         else:
             assert False, "please specify a supported architecture"
 
         print("DEBUG: model initialized")
+
+        # print(config_translated)
 
         lr = config["learning_rate"]
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
@@ -53,31 +65,45 @@ class training_iterator:
         print("DEBUG: iterator initialization finished")
 
     def iterate(self):
+        batch_size = self.batch_size
+        criterion = torch.nn.MSELoss()
+        mae_crit = torch.nn.L1Loss()
+
+        final = False
+
         if self.architecture == "PatchTST":
+
             self.model.train()
 
-            criterion = torch.nn.MSELoss()
-            mae_crit = torch.nn.L1Loss()
+            iterators.PatchTST_Iterator.optimize(self.model, self.optimizer, self.train, batch_size, self.data_dim,
+                                                 self.out_dim, criterion, self.pred_len, self.seq_len)
 
-            def closure():
-                torch.cuda.empty_cache()
-                self.optimizer.zero_grad()
-                x, y = utils_ba.get_single(self.train, self.pred_len, self.seq_len, self.data_dim, self.out_dim)
-                out = self.model.forward(x)
-                out = out[:, :, self.data_dim - self.out_dim:]
-                loss = criterion(out, y)
-                loss.backward()
-                return loss
+            self.iter += 1
+            if self.iter == self.iterations:
+                final = True
 
-            self.optimizer.step(closure)
+            self.model.eval()
 
-            ret_dict = {}
+            with torch.no_grad():
+                ret_dict = iterators.PatchTST_Iterator.predict(self.model, self.train, batch_size, self.data_dim,
+                                                               self.out_dim, criterion, mae_crit, self.pred_len,
+                                                               self.seq_len, final=final)
+
+
+
+        elif self.architecture == "Autoformer":
+
+            self.model.train()
+
+            iterators.Autoformer_Iterator.optimize(self.model, self.optimizer, self.train, batch_size, self.data_dim,
+                                                   self.out_dim, criterion, self.pred_len, self.seq_len)
 
             self.model.eval()
             with torch.no_grad():
                 x, y = utils_ba.get_single(self.val, self.pred_len, self.seq_len, self.data_dim, self.out_dim)
 
-                out = self.model.forward(x)
+                # out = self.model.forward(x)
+                out = utils_ba.split_prediction_autoformer(self.model, x, batch_size)
 
                 out = out[:, :, self.data_dim - self.out_dim:]
                 loss = criterion(out, y)
@@ -96,23 +122,36 @@ class training_iterator:
             self.iter += 1
             if self.iter == self.iterations:
                 ret_dict.update(self.finalize())
-                print(ret_dict)
 
         return ret_dict
 
     def finalize(self):
+        batch_size = self.batch_size
         truth = torch.squeeze(self.val[0, :, :])
         if self.architecture == "PatchTST":
             x, _ = utils_ba.get_single_pred(self.val, self.pred_len, self.seq_len, self.data_dim, self.out_dim)
             zeros = torch.zeros_like(self.val[0, :, :])
             with torch.no_grad():
-                prediction = self.model.forward(x)
+
+                prediction = utils_ba.split_prediction_single(self.model, x, batch_size)
+
             prediction = prediction[0, :, :]
             zeros[self.seq_len: self.pred_len + self.seq_len, :] = prediction
             prediction = torch.squeeze(zeros)
             return data_plotter.log_pred_plots(prediction.cpu(), truth.cpu())
 
-            #wandb.log({"truth": truth, "prediction": prediction})
+        elif self.architecture == "Autoformer":
+            x, _ = utils_ba.get_single_pred(self.val, self.pred_len, self.seq_len, self.data_dim, self.out_dim)
+            zeros = torch.zeros_like(self.val[0, :, :])
+            with torch.no_grad():
+
+                prediction = utils_ba.split_prediction_autoformer(self.model, x, batch_size)
+
+            prediction = prediction[0, :, :]
+            zeros[self.seq_len: self.pred_len + self.seq_len, :] = prediction
+            prediction = torch.squeeze(zeros)
+            return data_plotter.log_pred_plots(prediction.cpu(), truth.cpu())
+        # wandb.log({"truth": truth, "prediction": prediction})
 
     def fin(self):
         print("max_iter: " + self.iter.__str__())
