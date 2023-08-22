6ad594a6e58f9bfdc2c05423651c5db24040cb7e
N <thorbencomes@protonmail.com>
2023-08-22 13:26:54 +0200

    updated methods

--- iterators/PatchTST_Iterator.py
+++ iterators/PatchTST_Iterator.py
@@ -1,10 +1,14 @@
-import utils_ba
 import torch
 import data_plotter
+import utils.input_provider
+import utils.prediction
+import utils.dict_worker
+
 
 def optimize(model, optimizer, data, batch_size, data_dim, out_dim, criterion, pred_len, seq_len):
 
-    x, y = utils_ba.get_single_pred(data, pred_len, seq_len, data_dim, out_dim)
+    # x, y = utils_ba.get_single_pred(data, pred_len, seq_len, data_dim, out_dim)
+    x, y = utils.input_provider.get_input(data, pred_len, seq_len, data_dim, out_dim)
 
     def split_prediction_single():
 
@@ -13,42 +17,37 @@ def optimize(model, optimizer, data, batch_size, data_dim, out_dim, criterion, p
             j = i
 
             def closure():
+
+                optimizer.zero_grad()
                 out = model.forward(x[batch_size * j: batch_size * (j + 1), :, :])
                 out = out[:, :, data_dim - out_dim:]
                 y_cut = y[batch_size * j: batch_size * (j + 1), :, :]
                 loss = criterion(out, y_cut)
-                loss.backward
+                loss.backward()
                 return loss
+
             optimizer.step(closure)
 
     split_prediction_single()
 
 
-def predict(model, data, batch_size, data_dim, out_dim, criterion, mae_crit, pred_len, seq_len, final=False):
-    x, y = utils_ba.get_single(data, pred_len, seq_len, data_dim, out_dim)
+def predict(model, data, train_data, batch_size, data_dim, out_dim, criterion, mae_crit, pred_len, seq_len, final=False):
 
-    # out = self.model.forward(x)
-    out = utils_ba.split_prediction_single(model, x, batch_size)
-
+    x, y = utils.input_provider.get_input(data, pred_len, seq_len, data_dim, out_dim)
+    out = utils.prediction.split_prediction(model, x, batch_size)
     out = out[:, :, data_dim - out_dim:]
-    loss = criterion(out, y)
-    mae = mae_crit(out, y)
+    ret_dict = utils.dict_worker.pred_dict(out, y, criterion, mae_crit)
 
-    rmse = torch.sqrt(loss)
-    rmae = torch.sqrt(mae)
-
-    ret_dict = {
-        'validation_loss': loss,
-        'rmse': rmse,
-        'mae': mae,
-        'rmae': rmae
-    }
+    x, y = utils.input_provider.get_input(train_data, pred_len, seq_len, data_dim, out_dim)
+    out = utils.prediction.split_prediction(model, x, batch_size)
+    out = out[:, :, data_dim - out_dim:]
+    ret_dict.update(utils.dict_worker.pred_dict(out, y, criterion, mae_crit, train=True))
 
     if final:
 
         truth = torch.squeeze(data[0, :, :])
-        x, _ = utils_ba.get_single_pred(data, pred_len, seq_len, data_dim, out_dim)
-        prediction = utils_ba.split_prediction_single(model, x, batch_size)
+        x, _ = utils.input_provider.get_input(data, pred_len, seq_len, data_dim, out_dim, prediction=True)
+        prediction = utils.prediction.split_prediction(model, x, batch_size)
 
         zeros = torch.zeros_like(data[0, :, :])
         prediction = prediction[0, :, :]
